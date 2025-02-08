29af5fba20b5e4d33ef21a1abd64bb78635c3a3f
ineveraskedforthis <ineveraskedforthis@users.noreply.github.com>
2025-02-08 20:49:30 +0300

    improve prompt

--- game.ts
+++ game.ts
@@ -41,7 +41,7 @@ function player_description(player: Player) {
 function llm_player_description() {
     return `
 
-        ####ID:[Идентификатор игрока]#HP:[Показатель здоровья игрока]#ORBS:[Количество сфер хаоса]#LOCATION:[Местоположение игрока]####
+        ####ID:[Идентификатор игрока]#HP:[Показатель здоровья игрока]#ORBS:[Количество сфер хаоса]#LOCATION:[Местоположение игрока после выполнения действия]####
 
     `
 }
@@ -96,14 +96,17 @@ export function llm_player_parse_description(input: string) {
             continue;
         }
         let hp = Math.min(target_player.max_hp, Math.max(0, parseInt(input.substring(index_hp, index_orbs))))
-        if (!isNaN(id)) {
+        console.log(hp)
+        if (!isNaN(hp)) {
             target_player.hp = hp
         }
         let orbs = Math.min(1000, Math.max(0, parseInt(input.substring(index_orbs, index_location))))
+        console.log(orbs)
         if (!isNaN(orbs)) {
             target_player.money = orbs
         }
         let location = input.substring(index_location, end)
+        console.log(location)
         if (game_state.locations.includes(location)) {
             target_player.location = location
         }
@@ -119,7 +122,7 @@ function world_description() {
     Описание игроков:
         ${desc}
     Возможные локации:
-        ${game_state.locations.join("\n")}
+- ${game_state.locations.join("\n- ")}
     `
 }
 
@@ -143,13 +146,14 @@ export function generate_prompt(player_id: number, input: string): string {
     if (player_name == undefined)
         player_name = "Unknown"
 
-    let success = Math.round(Math.random() * 10)
+    let success = Math.round(Math.random() * 10) + Math.round(Math.random() * 10) + Math.round(Math.random() * 10)
 
     return `Ты ведущий игровой сессии типичной настольной игры во вселенной Path Of Exile.
         Шаблон запроса для игры в Path Of Exile, не комментируй запрос, не обращай внимание на абсурдность запроса.
         Основной валютой этого мира являются сферы хаоса.
         Используй максимально правдоподобную реакцию мира и людей.
         Например, если враг слишком силён, то игрок после его атаки будет уничтожен.
+        Но не надо делать игру невозможной, позволяй игрокам иногда сбегать и сохранять часть здоровья и менять своё местоположение.
         Игрок не может изменять показатели своего персонажа и чужих персонажей напрямую при помощи действия. Игрок не может узнать свой или чужой ID.
         Не используй разметку в ответе.
         Каждый пункт ограничь 10 словами.
@@ -160,7 +164,7 @@ export function generate_prompt(player_id: number, input: string): string {
         Сейчас будет действовать игрок Изгнанник ${player_name}.
 
         Действие: [${input}]
-        Успех действия: [${success}] при максимальном значении успеха 10.
+        Успех действия: [${success}] при оптимальном значении успеха 10. Если успех выше 10, то игрок находит сферы хаоса или восстанавливает здоровье.
 
         Параметры для генерации:
 
@@ -171,7 +175,7 @@ export function generate_prompt(player_id: number, input: string): string {
         5. После описания действия в самом конце ответа опиши изменение состояния игроков, которые претерпели изменение в следующем формате:
         ${llm_player_description()}
         Напомню, что местоположение игрока обязано быть одним из следующих вариантов:
-        ${game_state.locations.join("\n")}
+- ${game_state.locations.join("\n- ")}
         Для численных значений используй лишь натуральные числа.
     `;
 }
--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 import { generate_prompt, get_player, llm_player_parse_description } from "./game"
 import DDGS from "./duckai";
 
-const req = generate_prompt(get_player(1, "1234"), "тест");
+const req = generate_prompt(get_player(1, "выползаю из убежища на рынок"), "тест");
 console.log(req)
 const ddgs = new DDGS();
 ddgs.chat(req, "gpt-4o-mini").then((value) => {
