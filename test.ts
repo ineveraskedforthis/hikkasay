ee99bd423da7429f0b4fd602cc2df104a8fc12d8
ineveraskedforthis <ineveraskedforthis@users.noreply.github.com>
2025-02-09 12:10:15 +0300

    improve prompt

--- test.ts
+++ test.ts
@@ -1,12 +1,26 @@
 import { generate_prompt, get_player, llm_player_parse_description } from "./game"
 import DDGS from "./duckai";
+let player = get_player(1, "Тестовый");
+{
+    const req = generate_prompt(player, "Покупаю самоцвет на рынке");
+    // console.log(req)
+    const ddgs = new DDGS();
+    ddgs.chat(req, "gpt-4o-mini").then((value) => {
+        llm_player_parse_description(value)
+        console.log(value)
+        {
+            const req = generate_prompt(player, "Использую самоцвет");
+            // console.log(req)
+            const ddgs = new DDGS();
+            ddgs.chat(req, "gpt-4o-mini").then((value) => {
+                llm_player_parse_description(value)
+                console.log(value)
+            }).catch((reason) => {
+                console.log(reason)
+            });
+        }
+    }).catch((reason) => {
+        console.log(reason)
+    });
+}
 
-const req = generate_prompt(get_player(1, "Тестовый"), "тренирую тело и разум");
-console.log(req)
-const ddgs = new DDGS();
-ddgs.chat(req, "gpt-4o-mini").then((value) => {
-    llm_player_parse_description(value)
-    console.log(value)
-}).catch((reason) => {
-    console.log(reason)
-});
